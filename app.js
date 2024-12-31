4f7c2706e95ccd097734e5b9de1eefbcd81e5039
soomin <s2305@e-mirim.hs.kr>
2024-12-31 21:21:47 +0900

    [2-4] var, let, const

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi");
+var a = 5;
+const b = 2;
+let myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
+
+var a = 1;
+myName = "nicolas";
+console.log("your new name is " + myName);
