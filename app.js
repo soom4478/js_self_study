78c7e64890c27545fdfbcb0bdaadb5587902c8c3
soomin <s2305@e-mirim.hs.kr>
2025-01-03 01:28:58 +0900

    [3-3] Event

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const title = document.getElementsByTagName("h1");
-const title2 = document.querySelector(".hello h1"); // CSS 형식으로, 1개만
-const title3 = document.querySelectorAll(".hello h1"); // 모두 가져옴, array로 반환
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
-console.log(title2);
-console.log(title3);
+function handleTitleClick() {
+    title.style.color = "blue";
+}
 
-const title4 = document.getElementById("hello");
-const title5 = document.querySelector("#hello"); // 동일하지만 querySelector는 하위 요소를 가져오기 더 좋음
-
-console.log(title4);
-console.log(title5);
+title.addEventListener("click", handleTitleClick);
