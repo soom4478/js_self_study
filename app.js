2c8e8905335fe885338b35f70c5277f03eb41ee1
soomin <s2305@e-mirim.hs.kr>
2025-01-03 01:06:01 +0900

    [3-1] HTML in Javascript

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-const age = parseInt(prompt("How old are you?")); // NaN이 아니면 숫자 그 외에는 숫자 아님
+const title = document.getElementById("title");
 
-if(isNaN(age) || age < 0) {
-    console.log("Please write a number");
-}
-else if(age < 18) {
-    console.log("You are too young");
-}
-else if(age >= 18 && age <= 50) {
-    console.log("You can drink");
-}
-else if(age > 50 && age <= 80) {
-    console.log("You should exercise");
-}
-else if(age === 100) {
-    console.log("wow you are wise");
-}
-else if(age > 80) {
-    console.log("You cand do whatever you want");
-}
+console.log(title);
+
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
