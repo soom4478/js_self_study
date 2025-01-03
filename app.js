7fe85c39539f2b392a7f571d63106eb5d018e48f
soomin <s2305@e-mirim.hs.kr>
2025-01-04 00:40:22 +0900

    [3-8] css in js 3 (toggle)

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass); // classList를 이용하면 기존 class가 사라지지 않는다
-    }
-    else {
-        h1.classList.add(clickedClass);
-    }
-    
+    h1.classList.toggle("clicked");
+    // toggle : classList에 clicked가 있는지 확인 후
+    // 있다면 제거, 없다면 추가
 }
 
 h1.onclick = handleTitleClick;
