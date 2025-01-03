deee04727838d71977f2af7e5c571c53271cbf54
soomin <s2305@e-mirim.hs.kr>
2025-01-04 00:35:20 +0900

    [3-8] css in js 3 (classLIst)

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
     const clickedClass = "clicked";
-    if (h1.className === clickedClass) {
-        h1.className = "";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass); // classList를 이용하면 기존 class가 사라지지 않는다
     }
     else {
-        h1.className = clickedClass; // css를 이용하는 게 js에 직접 넣는 것보다 좋음
+        h1.classList.add(clickedClass);
     }
     
 }
