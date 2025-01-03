fea899f810fb820dd996deae3a47a9cece723b44
soomin <s2305@e-mirim.hs.kr>
2025-01-04 00:33:01 +0900

    [3-7] css in js 2

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass) {
+        h1.className = "";
     }
     else {
-        newColor = "blue";
+        h1.className = clickedClass; // css를 이용하는 게 js에 직접 넣는 것보다 좋음
     }
-    h1.style.color = newColor;
+    
 }
 
 h1.onclick = handleTitleClick;
