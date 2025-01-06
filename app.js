99dc6f0b04c68b1d0deff6e4a7c3135f9ec93f5b
soomin <s2305@e-mirim.hs.kr>
2025-01-07 00:53:33 +0900

    [4-6] localStorage

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    // 함수를 실행할 때 event object를 argument로 줌
-    event.preventDefault(); // 기본 동작이 샐행되는 걸 막아줌
-    console.log(event);
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME); // local storage API
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-function handleLinkClick(event) {
-    event.preventDefault(); // 기본 동작 실행 막음
-    console.log(event);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
-// 함수를 실행시키는 건 브라우저저, 그러나 기본 동작작을 막아야할 때가 있음(정보를 얻거나 할 때)
-// 첫 번째 argument에 실행한 이벤트의 정보가 들어간 오브젝트를 줌
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    paintGreetings(savedUsername);
+}
